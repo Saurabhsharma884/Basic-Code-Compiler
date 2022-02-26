d12249e4470bfe16ad06f60c9a9cba762d045d62
Saurabh <apple@192.168.1.8>
2022-02-26 16:12:57 +0530

    API

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
+function init() {
+  var submitBtn = document.getElementById("submitBtn");
+  submitBtn.addEventListener("click", getData);
+}
 
-var submitBtn = document.getElementById("submitBtn");
-var languageCode 
-var langDropDown =document.getElementById('langs');
+var languageCode;
+var codeString;
 
-submitBtn.addEventListener("click", function (e) {
-    languageCode = langDropDown.options[langDropDown.selectedIndex].value
-console.log(languageCode);
-});
+function getData() {
+  var langDropDown = document.getElementById("langs");
+  var textArea = document.getElementById("editorArea");
+  languageCode = langDropDown.options[langDropDown.selectedIndex].value;
+  codeString = textArea.value;
+
+  submitCode();
+}
+
+function submitCode() {
+    var data = { code: `${codeString}`, langId: `${languageCode}` };
+  var postReq = new XMLHttpRequest();
+  postReq.open("POST", " https://codequotient.com/api/executeCode", true);
+  postReq.setRequestHeader("Content-Type", "application/json");
+  
+  console.log(JSON.stringify(data));
+  postReq.send(JSON.stringify(data));
+
+  postReq.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var response = JSON.parse(postReq.responseText);
+      console.log(response);
+    }
+  };
+}
+
+init();
